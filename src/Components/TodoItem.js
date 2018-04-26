234980acbe579a8083f22574ad1cf6266162d937
Your Name <you@example.com>
2018-04-26 14:51:46 -0400

    brought in a todo list from an api

---
+++ src/Components/TodoItem.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react'
+
+class TodoItem extends Component {
+  render() {
+    return (
+      <li className="Todo">
+        <strong> {this.props.todo.title}</strong>:
+      </li>
+    )
+  }
+}
+
+export default TodoItem
