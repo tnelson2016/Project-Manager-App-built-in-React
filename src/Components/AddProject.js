0d6aa158a65f427661a499cd688768fb07c740e0
Your Name <you@example.com>
2018-04-26 00:37:03 -0400

    We now have the ability to add projects!

--- src/Components/AddProject.js
+++ src/Components/AddProject.js
@@ -1,17 +1,39 @@
 import React, { Component } from 'react'
 
 class AddProject extends Component {
+  constructor() {
+    super()
+    this.state = {
+      newProject: {}
+    }
+  }
+
   static defaultProps = {
     categories: ['Web Design', 'Web Development', 'Mobile Development']
   }
   handleSubmit(e) {
-    console.log('Submitted')
+    if (this.refs.title.value === '') {
+      alert('Title is required')
+    } else {
+      this.setState(
+        {
+          newProject: {
+            title: this.refs.title.value,
+            category: this.refs.category.value
+          }
+        },
+        function() {
+          // console.log(this.state)
+          this.props.addProject(this.state.newProject)
+        }
+      )
+    }
     e.preventDefault()
   }
   render() {
     let categoryOptions = this.props.categories.map(category => {
       return (
-        <option key={category} value="category">
+        <option key={category} value={category}>
           {category}
         </option>
       )
@@ -19,7 +41,7 @@ class AddProject extends Component {
     return (
       <div>
         <h3>Add Project</h3>
-        <form onSubmit={this.handleSubmit}>
+        <form onSubmit={this.handleSubmit.bind(this)}>
           <div>
             <label>Title</label>
             <br />
